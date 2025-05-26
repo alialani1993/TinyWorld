eed56e82390a539c0ea82826a795b553808cda8b
Snow <157506648+Vxvxds@users.noreply.github.com>
2025-05-26 00:09:45 -0500

    scripts
    made scripts (hoping they work right) and fixed office rendering

---
+++ Assets/Scripts/UIScripts/CreditsPoster.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CreditsPoster : MonoBehaviour
+{
+    [SerializeField] public UnityEvent OpenCredits;
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private List<Material> _defaultMat;
+    [SerializeField] private List<Material> _mat;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    private void OnMouseOver()
+    {
+        _meshRenderer.SetMaterials(_mat);
+        if (Input.GetMouseButtonDown(0))
+        {
+            OpenCredits.Invoke();
+            _meshRenderer.SetMaterials(_defaultMat);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        _meshRenderer.SetMaterials(_defaultMat);
+    }
+}
