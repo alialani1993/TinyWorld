7ca8dbd61da9a30a6bb96c1218d54da0aa0c558e
Ali <aliahmadalani@gmail.com>
2025-05-26 00:19:13 -0500

    Merge branch 'main' of https://github.com/alialani1993/TinyWorld

---
+++ Assets/Scripts/UIScripts/Highlight.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Highlight : MonoBehaviour
+{
+    [SerializeField] public UnityEvent StartGame;
+    [SerializeField] private SkinnedMeshRenderer _skinneMeshRenderer;
+    [SerializeField] private List<Material> _defaultMat;
+    [SerializeField] private List<Material> _mat;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    private void OnMouseOver()
+    {
+        _skinneMeshRenderer.SetMaterials(_mat);
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGame.Invoke();
+            _skinneMeshRenderer.SetMaterials(_defaultMat);
+            Destroy(this);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        _skinneMeshRenderer.SetMaterials(_defaultMat);
+    }
+}
